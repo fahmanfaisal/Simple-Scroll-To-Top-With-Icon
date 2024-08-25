4bca66905c275728f2a94a320a2bad0c60e29e2a
fahmanfaisal <fahmanchy@gmail.com>
2024-08-26 01:16:54 +0600

    named

--- README.md
+++
@@ -1,1 +0,0 @@
-# Simple-Scroll-To-Top-With-Icon
--- simple-scroll-to-top-with-icon.php
+++ simple-scroll-to-top-with-icon.php
@@ -4,5 +4,13 @@
  * Plugin name: Simple Scroll To Top With Icon
  * Plugin Uri: https://wordpress.org/plugins/simple-scroll-to-top-with-icon/
  * Description: Easily add a scroll to top button with a customizable icon to your WordPress site. Enhance user experience by allowing visitors to smoothly scroll back to the top of the page with a single click.
- * 
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Author: MD Faisal Chy
+ * Author URI: https://scriptnotion.com/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI: https://github.com/fahmanfaisal/Simple-Scroll-To-Top-With-Icon
+ * Text Domain: sstti
  */
