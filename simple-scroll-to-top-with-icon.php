f29d0e7ef13b60b804be56187c0051e9cd5e5550
fahmanfaisal <fahmanchy@gmail.com>
2024-08-28 00:23:26 +0600

    plugin-activation

--- simple-scroll-to-top-with-icon.php
+++ simple-scroll-to-top-with-icon.php
@@ -14,3 +14,35 @@
  * Update URI: https://github.com/fahmanfaisal/Simple-Scroll-To-Top-With-Icon
  * Text Domain: sstti
  */
+
+
+// Including CSS
+function sstti_enqueue_style()
+{
+    wp_enqueue_style('sstti-style', plugins_url('css/sstti-style.css', __FILE__));
+}
+add_action('wp_enqueue_scripts', 'sstti_enqueue_style');
+
+// Including JS
+function sstti_enqueue_scripts()
+{
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('sstti-plugin-script', plugins_url('js/sstti-plugin.js', __FILE__), array(), '1.0.0', 'true');
+}
+add_action('wp_enqueue_scripts', 'sstti_enqueue_scripts');
+
+// jQuery Plugin Setting Activation
+function sstti_scroll_script()
+{
+?>
+    <script>
+        jQuery(document).ready(function($) {
+            // jQuery('.bck').backToTop();
+            jQuery('.bck').backToTop({
+                fxName: 'rightToLeft'
+            });
+        });
+    </script>
+<?php
+}
+add_action('wp_footer', 'sstti_scroll_script');
